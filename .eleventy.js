24331e506dc741aaf580ee9386161bfc8f8693c3
Rick Henry <rickhenry@rickhenry.dev>
2022-06-09 13:22:31 -0400

    move font local
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc40e88ad96c513c411aebbcca53b8924b3595c1431677a9749f5a0c420eef64
-size 1680
+oid sha256:05c52d788886674429a988a4acca8ac99f35ba520f111bb8b0d58a632b27e644
+size 2036
