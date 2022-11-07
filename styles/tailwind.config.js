9e683496f8317d02849d8ae3f330629495058bd0
Rick Henry <rickhenry@rickhenry.dev>
2022-11-07 02:42:01 -0500

    remove lfs completely...hopefully
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:de1d7958bd57ad2c8d5f54de340eea79a896cd12eeedeb295fb6dd3b0821dcc2
-size 309
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./**/*.njk",
+    "./**/*.md",
+    "./**/*.css"
+  ],
+  theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens
+    },
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+  ],
+};
