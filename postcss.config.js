4e7ad86b64d62a917698d2d5799c9320a48eabf9
Rick Henry <rickhenry@rickhenry.dev>
2022-11-07 19:12:44 -0500

    always cssnano
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,8 @@ module.exports = {
       config: "./styles/tailwind.config.js",
     },
     autoprefixer: {},
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+    cssnano: {
+      preset: 'default'
+    },
   },
 };
